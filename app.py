345b677dd37aae5b6d6fdd9e4d9121b933d76470
Epchannel <epchannel1208@gmail.com>
2023-12-24 21:37:41 +0700

    update deploy to vps

--- app.py
+++ app.py
@@ -13,67 +13,10 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-
-# Khởi tạo FastAPI app
-app = FastAPI()
-
-# Định nghĩa request model
-class Query(BaseModel):
-    question: str
-
-# Endpoint để xử lý PDF tải lên hoặc URL
-@app.post("/upload_pdf/")
-async def upload_pdf(pdf_file: UploadFile = File(...), pdf_url: Optional[str] = Form(None)):
-    global db, chain  # Khai báo để sử dụng biến toàn cục
-    if pdf_url:
-        response = requests.get(pdf_url)
-        filename = "downloaded_pdf.pdf"
-        with open(filename, 'wb') as f:
-            f.write(response.content)
-    else:
-        filename = pdf_file.filename
-        with open(filename, 'wb') as f:
-            f.write(await pdf_file.read())
-
-    return {"filename": filename}
-
-# API endpoint
-@app.post("/ask")
-async def ask_question(query: Query):
-    try:
-        response = chain.run(query.question)
-        return response
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-@app.get("/", response_class=HTMLResponse)
-def read_root():
-    with open('index.html', 'r') as file:
-        html_content = file.read()
-    return HTMLResponse(content=html_content)
-
-
-
-#Model
-"""Load Data & Model"""
-os.environ["OPENAI_API_KEY"] = "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"
-model = OpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
-
-# Xử lý PDF tại đây
-pdf_loader = UnstructuredPDFLoader(filename)
-pdf_pages = pdf_loader.load_and_split()
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=64)
-texts = text_splitter.split_documents(pdf_pages)
-
-# Tạo Embeddings và Vectorstores
-MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
-hf_embeddings = HuggingFaceEmbeddings(model_name=MODEL_NAME)
-db = Chroma.from_documents(texts, hf_embeddings, persist_directory="db")
-
+db = None
+chain = None
 
 """#Use a Chain"""
-
 custom_prompt_template = """Tôi muốn bạn đóng vai trò là Botchat PDF, được phát triển bởi nhóm sinh viên Khoa Công nghệ thông tin, Trường Đại học Mỏ - Địa chất.
 Bạn sẽ đưa ra câu trả lời từ ngữ cảnh và câu hỏi dưới đây.
 Câu trả lời của bạn phải đầy đủ, chính xác và mô tả chi tiết về nội dung câu hỏi của người dùng.
@@ -86,7 +29,6 @@ Question: {question}
 
 """
 
-from langchain import PromptTemplate
 def set_custom_prompt():
     """
     Prompt template for QA retrieval for each vectorstore
@@ -95,13 +37,72 @@ def set_custom_prompt():
                             input_variables=['context', 'question'])
     return prompt
 
-prompt = set_custom_prompt()
-chain = RetrievalQA.from_chain_type(
-    llm=model,
-    chain_type="stuff",
-    retriever=db.as_retriever(search_kwargs={"k": 2}),
-    chain_type_kwargs={'prompt': prompt}
-)
+# Khởi tạo FastAPI app
+app = FastAPI()
+
+# Định nghĩa request model
+class Query(BaseModel):
+    question: str
+
+# Endpoint để xử lý PDF tải lên hoặc URL
+@app.post("/upload_pdf/")
+async def upload_pdf(pdf_file: UploadFile = File(...), pdf_url: Optional[str] = Form(None)):
+    if pdf_url:
+        response = requests.get(pdf_url)
+        filename = "downloaded_pdf.pdf"
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+    else:
+        filename = pdf_file.filename
+        with open(filename, 'wb') as f:
+            f.write(await pdf_file.read())
+
+    # Xử lý PDF tại đây
+    process_pdf(filename)  # Xử lý file PDF sau khi tải lên
+    return {"filename": filename}
+
+def process_pdf(filename):
+    global db, chain
+    pdf_loader = UnstructuredPDFLoader(filename)
+    pdf_pages = pdf_loader.load_and_split()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=64)
+    texts = text_splitter.split_documents(pdf_pages)
+
+    # Tạo Embeddings và Vectorstores
+    MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+    hf_embeddings = HuggingFaceEmbeddings(model_name=MODEL_NAME)
+    db = Chroma.from_documents(texts, hf_embeddings, persist_directory="db")
+    
+    # Cấu hình và sử dụng chain ở đây nếu cần
+def initialize_chain():
+    global chain
+    if db is not None:
+        chain = RetrievalQA.from_chain_type(
+            llm=model,
+            chain_type="stuff",
+            retriever=db.as_retriever(search_kwargs={"k": 2}),
+            chain_type_kwargs={'prompt': prompt}
+        )
+
+#Model
+"""Load Data & Model"""
+os.environ["OPENAI_API_KEY"] = "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"
+model = OpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+# API endpoint
+@app.post("/ask")
+async def ask_question(query: Query):
+    try:
+        response = chain.run(query.question)
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    with open('index.html', 'r', encoding='utf-8') as file:  # Thêm 'encoding='utf-8'
+        html_content = file.read()
+    return HTMLResponse(content=html_content)
 
 
 # Hàm để chạy ứng dụng trên máy local
